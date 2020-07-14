fd3478dfa5c5ba991755f12efe825368d70c5777
Sam <samshum90@gmail.com>
2020-07-14 19:42:54 +0100

    added router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import  { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from '../Navigation';
 const App = () => (
-  <>
+  <Router>
     <h1>App</h1>
-  </>
+  </Router>
 );
 
 export default App;
