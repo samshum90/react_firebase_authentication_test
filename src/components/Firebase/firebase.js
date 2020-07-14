f44fd9b1565a1ff89d02e8fecbf21a3ac2f6bdaf
Sam <samshum90@gmail.com>
2020-07-14 20:53:12 +0100

    env has been set up

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,3 +1,5 @@
+import app from 'firebase/app'
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -5,4 +7,11 @@ const config = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+    }
 }
+export default Firebase;
