18e8a5a2d88993557f48165773bb213edd92d2f6
Sam <samhum90@gmail.com>
2020-07-15 13:23:49 +0100

    added secure routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import  { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation';
 import Landing from '../Landing';
@@ -9,20 +9,33 @@ import Home from '../Home';
 import Account from '../Account';
 import Admin from '../Admin';
 import * as ROUTES from '../../constants/routes'
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-      <Route exact path={ROUTES.LANDING} component={Landing} />
-      <Route path={ROUTES.SIGN_UP} component={SignUp} />
-      <Route path={ROUTES.SIGN_IN} component={SignIn} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-      <Route path={ROUTES.HOME} component={Home} />
-      <Route path={ROUTES.ACCOUNT} component={Account} />
-      <Route path={ROUTES.ADMIN} component={Admin} />
-    </div>
-  </Router>
-);
+    this.state = {
+      authUser: null,
+    };
+  }
 
+  render(){
+    return (
+  
+      <Router>
+        <div>
+          <Navigation authUser={this.state.authUser} />
+
+          <Route exact path={ROUTES.LANDING} component={Landing} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.SIGN_IN} component={SignIn} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+          <Route path={ROUTES.HOME} component={Home} />
+          <Route path={ROUTES.ACCOUNT} component={Account} />
+          <Route path={ROUTES.ADMIN} component={Admin} />
+        </div>
+      </Router>
+    );
+  }
+}
+  
 export default App;
